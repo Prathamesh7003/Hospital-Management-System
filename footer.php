6a41b467504ff8e997233fa4c19b096ade20be06
Prathamesh Prakash Agawane <122960045+Prathamesh7003@users.noreply.github.com>
2023-11-15 01:13:45 +0530

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,58 @@
+<?php
+
+echo '  <section id="contact">
+<div class="container">
+  <div class="row">
+    <div class="col-md-12 text-center">
+      <h2 class="section-title">Contact Us</h2>
+    </div>
+  </div>
+
+  <div class="row justify-content-center">
+    <div class="col-lg-6 col-md-7">
+      <div class="info">
+        <div>
+          <i class="fa fa-map-marker"></i>
+          <p>Wadgaonsheri<br>Pune-411014</p>
+        </div>
+
+        <div>
+          <i class="fa fa-envelope"></i>
+          <p>pa7337@gmail.com</p>
+        </div>
+
+        <div>
+          <i class="fa fa-phone"></i>
+          <p>+91 7097097003</p>
+        </div>
+
+      </div>
+    </div>
+
+  </div>
+</div>
+</section>
+
+<footer class="site-footer">
+<div class="bottom">
+  <div class="container">
+    <div class="row">
+
+      <div class="col-lg-6 col-xs-12 text-lg-left text-center">
+        <p class="copyright-text">
+          © Hospital
+        </p>
+        <div class="credits">
+          
+        
+         Presented by <a href="#">PA.Pvt.Ltd</a>
+        </div>
+      </div>
+
+      
+    </div>
+  </div>
+</div>
+</footer>';
+
+?>
